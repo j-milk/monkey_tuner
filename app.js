bf852880acf8bda2f9f02f428d58b6ab53591bde
j-milk <47408744+j-milk@users.noreply.github.com>
2019-04-11 15:23:57 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+function playHighe() {
+    var highe = new Audio("./GuitarChord-A/1st_String_E_64kb.mp3");
+    highe.currentTime = 0;
+    highe.play();
+   }
+
+function playB() {
+    var b = new Audio ("./GuitarChord-A/2nd_String_B__64kb.mp3");
+    b.currentTime = 0;
+    b.play();
+}
+
+function playG() {
+    var g = new Audio ("./GuitarChord-A/3rd_String_G_64kb.mp3");
+    g.currentTime = 0;
+    g.play();
+}
+
+function playD() {
+    var d = new Audio ("./GuitarChord-A/4th_String_D_64kb.mp3");
+    d.currentTime = 0;
+    d.play();
+}
+
+function playA() {
+    var a = new Audio ("./GuitarChord-A/5th_String_A_64kb.mp3");
+    a.currentTime = 0;
+    a.play();
+}
+
+function playLowe() {
+    var lowe = new Audio ("./GuitarChord-A/6th_String_E_64kb.mp3");
+    lowe.currentTime = 0;
+    lowe.play();
+}
